b6b9c0f10e172cbfdc0c06ee37e30538dcd11056
chlrkd7251 <rkdalsrbs0@gmail.com>
2024-09-29 02:59:41 +0900

    project init.

---
+++ target/debug/deps/file_system_monit-95d6163998310a09.d
@@ -0,0 +1,5 @@
+/home/polyroly/dev/mgit/file-system-monit/target/debug/deps/libfile_system_monit-95d6163998310a09.rmeta: src/main.rs
+
+/home/polyroly/dev/mgit/file-system-monit/target/debug/deps/file_system_monit-95d6163998310a09.d: src/main.rs
+
+src/main.rs:
