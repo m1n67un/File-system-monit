e9e3fd88c41766430cfb2d5e068c17f81f09e7b1
m1n67un <rkdalsrbs0@gmail.com>
2024-09-29 03:06:38 +0900

    main function update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use notify::{RecommendedWatcher, RecursiveMode, Watcher, Config};
+use std::path::Path;
+use syslog::{Facility, Formatter3164, BasicLogger};
+use log::{LevelFilter, warn};
+
 fn main() {
-    println!("Hello, world!");
+    let path = std::env::args().nth(1).expect("Argument 1 needs to be a path");
+
+    // syslog 설정
+    let formatter = Formatter3164 {
+        facility: Facility::LOG_USER,
+        hostname: None,
+        process: "fs-watcher".into(),
+        pid: 0,
+    };
+    let logger = syslog::unix(formatter).expect("could not connect to syslog");
+    log::set_boxed_logger(Box::new(BasicLogger::new(logger)))
+        .map(|()| log::set_max_level(LevelFilter::Warn))
+        .expect("could not register logger");
+
+    println!("Watching {}", path);
+    if let Err(e) = watch(path) {
+        println!("Error: {:?}", e);
+    }
+}
+
+fn watch<P: AsRef<Path>>(path: P) -> notify::Result<()> {
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    let mut watcher = RecommendedWatcher::new(tx, Config::default())?;
+
+    watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
+
+    for res in rx {
+        match res {
+            Ok(event) => log_event(event),
+            Err(e) => println!("Watch error: {:?}", e),
+        }
+    }
+
+    Ok(())
+}
+
+fn log_event(event: notify::Event) {
+    warn!("Change occurred: {:?}", event);
 }
